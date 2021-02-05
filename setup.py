fa18332219c4973435f6dc3c3db5c4da578f632f
Varun Premakantha <vpremakantha@gmail.com>
2021-02-05 00:12:20 +0000

    Production Prep: Added setup.py, and requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CountVision',
+    version='2021.05.01',
+    packages=[''],
+    url='',
+    license='MIT ',
+    author='VARCO',
+    author_email='',
+    description=''
+)
