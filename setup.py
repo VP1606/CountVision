c4df4a905398d6a110662bfed80dd0bdf020b623
Varun Premakantha <vpremakantha@gmail.com>
2021-02-07 18:32:30 +0000

    Fix: Included OpenCV-Contrib in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='2021.05.01',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
-    install_requires=['numpy', 'imutils', 'opencv-python', 'setuptools', 'cvlib', 'PyQt5', 'pynput', 'pyshortcuts'],
+    install_requires=['numpy', 'imutils', 'opencv-python', 'setuptools', 'cvlib', 'PyQt5', 'pynput', 'pyshortcuts', 'opencv-contrib-python'],
     url='https://github.com/VP1606/CountVision/tree/master',
     license='APACHE LICENSE V2.0',
     author='VARCO',
