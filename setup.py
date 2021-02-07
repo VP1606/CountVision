e3c0f51ea45617ea654b4ec01bf5ccffe5fd9b95
Varun Premakantha <vpremakantha@gmail.com>
2021-02-07 15:39:32 +0000

    Added: Setup.py with dependencies.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CountVision',
+    version='2021.05.01',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    install_requires=['numpy', 'imutils', 'opencv-python', 'setuptools', 'cvlib', 'PyQt5', 'pynput'],
+    url='https://github.com/VP1606/CountVision/tree/master',
+    license='APACHE LICENSE V2.0',
+    author='VARCO',
+    author_email='vpremakantha@gmail.com',
+    description='Computer Vision Project',
+)
