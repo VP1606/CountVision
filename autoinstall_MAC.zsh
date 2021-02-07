f7dadb6541f8cf5d8ba7698bbe2501490c59229c
Varun Premakantha <vpremakantha@gmail.com>
2021-02-07 21:12:51 +0000

    Added: Automated Install Script (change line 17 to specific target when releasing).
    Now entered Version 2021.05.02

---
+++ autoinstall_MAC.zsh
@@ -0,0 +1,27 @@
+#!/bin/zsh
+
+echo "------------------------------------------------"
+echo "----------- COUNTVISION 2021.05.02e ------------"
+echo "--- Automated Install Sequence for MACOS -------"
+echo "------------------------------------------------"
+
+pip3 install virtualenv
+
+echo "Creating Virtual Run Environment (2/6)"
+virtualenv venv
+
+echo "Entering VENV (3/6)"
+source venv/bin/activate
+
+echo "Cloning Version 2021.05.02e (4/6)"
+git clone -b release/2021.05.02e --single-branch https://github.com/VP1606/CountVision.git
+
+cd CountVision
+
+echo "Installing Dependencies (5/6)"
+pip3 install -e .
+
+echo "Handing over to on-board install scripts (6/6)"
+python3 prepare.py
+
+echo "AIS Complete"
